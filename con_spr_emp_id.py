3fef65250d84041523a52b69994643ebfebc32fd
sabbah128 <sabbah128@gmail.com>
2024-02-23 05:23:57 -0800

    Update con_spr_emp_id.py

--- con_spr_emp_id.py
+++ con_spr_emp_id.py
@@ -1,5 +1,7 @@
 
 import pypyodbc as odbc
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 driver = 'SQL SERVER'
@@ -12,22 +14,36 @@ conn = f"""
     DATABASE={db_name};
     Trust_Connection=yes;
     """
+
 connection = odbc.connect(conn)
+cursor = connection.cursor()
 print('\n*** SQL Server is connected. ***')
 
 print('\nempid, shipperid, freight, shipcountry')
-cursor = connection.cursor()
 query = 'SELECT TOP (5) empid, shipperid, freight, shipcountry FROM Sales.Orders'
 cursor.execute(query)
 rows = cursor.fetchall()
 for row in rows:
     print(row)
 
-emp_id = 50
+emp_id = 2
 query = f'EXECUTE spr_emp_id {emp_id};'
 cursor.execute(query)
 rows = cursor.fetchall()
 print('\nSum of freight: ', sum(r_col[1] for r_col in rows))
+
+query = 'SELECT * FROM Sum_freight'
+cursor.execute(query)
+rows_veiw = cursor.fetchall()
+print()
+for row in rows_veiw:
+    print(row)
+
+rows_value = np.array(rows_veiw)
+
+fig = plt.figure(figsize = (4, 3))
+plt.bar(rows_value[:, 0], rows_value[:, 1], width=0.5)
+plt.show()
 
 connection.close()
 
